59cb38bd23044ebc7c6c676a6ddd1245049e870b
unknown <brandonrodriguez_93@email.com>
2020-10-26 12:34:27 -0600

    me cago en pablo

--- Quiz #2/Ejercicio#1.py
+++ Quiz #2/Ejercicio#1.py
@@ -19,50 +19,24 @@ temblores registrados en ese año y el promedio de los temblores registrados en 
 class Lista():
            
     def crearList(self):
+        lista = []
+        dic = {}
         temblores = open("temblores1.csv","r+", encoding='utf-8-sig')
-        #newfile = open("test.txt","w+")
-        mes = 0
-        anio = 0
-        tem_mag = 0.0
-        year = []
-        years = 0
-        month = []
-        months = 0
-        total = 0
-        total1 = 0
-        #read the whole document linea by line and save in list
         for linea in temblores:
             linea1 = linea.replace(",",".")
-            linea2 = linea1.replace(";",",").strip() #['2015;1;4,0','2020;1;4,1','2020;7;1,1']
-            #create constants with year, month, mangnitud for current loop
-            a_year = linea2.split(',')[0] # 2015
-            a_month = linea2.split(',')[1] # 1
-            a_magnitud = linea2.split(',')[2] #4.0
-            
-            b_year = linea2.split(',')[0] # 2015
-            b_month = linea2.split(',')[1] # 1
-            b_magnitud = linea2.split(',')[2] #4.0
-            
-            if a_year == b_year and a_month == b_month:
-                total = float(a_magnitud) + float(b_magnitud)
-                total1 = total1 + total
-                print("Year: ", a_year)
-                print("Month: ", a_month)
-                print("Total Magnitud:", total)
+            linea2 = linea1.replace(";",",").strip()
+            year = linea2.split(',')[0] # 2015
+            month = linea2.split(',')[1] # 1
+            magnitud = linea2.split(',')[2] #4.0
+            if year in dic:
+                dic[year].append(float(magnitud))
             else:
-                continue
-
-                
-            
-            #construct list with unique years in document
-            #years = linea2.split(',')[0] 
-            #year.append(years)
-            #my_year = set(year) #['2015','2020']  
-            
-            #construct unique months in document
-            #months = linea2.split(',')[1]
-            #month.append(months)
-            #my_month = set(month) # ['1','7']
-            
+                dic[year] = [float(magnitud)]
+        print(dic)
+        
+        for i in dic:
+            suma= sum(dic.get(i))
+            print(suma)
+  
 generar = Lista()
 generar.crearList()
