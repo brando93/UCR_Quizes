0de20036391a00554064f0301c8cddcd6d7dee3e
unknown <brandonrodriguez_93@email.com>
2020-10-28 16:56:52 -0600

    removing 1y2

--- Quiz #2/Ejercicio#2.py
+++
@@ -1,6 +0,0 @@
-"""
-2 - Ejercicio 2 (20 puntos)
-Escribir dos funciones que permitan calcular:
-a) La duración en segundos de un intervalo dado en horas, minutos y segundos.
-b) La duración en horas, minutos y segundos de un intervalo dado en segundos.
-"""
