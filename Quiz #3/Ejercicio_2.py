2d263f4f0d7466366f3cadcbf8438e6ecd73b63a
unknown <brandonrodriguez_93@email.com>
2020-11-04 11:00:33 -0600

    me cago en pablo

--- Quiz #3/Ejercicio_2.py
+++ Quiz #3/Ejercicio_2.py
@@ -1,9 +1,11 @@
 """
 Ejercicio 2 (60 puntos)
 Simulador de  Hacienda.
-Cree un programa que simule el sistema nacional de tributación, en hacienda existen Empresas y Personas que tributan generalmente el 13% de sus ingresos.
+Cree un programa que simule el sistema nacional de tributación, en hacienda existen Empresas y Personas que 
+tributan generalmente el 13% de sus ingresos.
 
-Para registrar un contribuyente se necesita el número de cédula (jurídica o física),  el nombre y el teléfono, la información de las personas registradas debe leerse  por pantalla.
+Para registrar un contribuyente se necesita el número de cédula (jurídica o física),  el nombre y el teléfono, 
+la información de las personas registradas debe leerse  por pantalla.
 
 Cree un programa que permita:
 
@@ -12,7 +14,8 @@ Cree un programa que permita:
 2.	Teléfono.
 
 2- Registrar ventas.
-Permita un diccionario como llave ID del vendedor registrar facturas de ventas de un vendedor, donde se registra una clase factura el cual debe detallar.
+Permita un diccionario como llave ID del vendedor registrar facturas de ventas de un vendedor, 
+donde se registra una clase factura el cual debe detallar.
 -Id vendedor
 -Fecha factura
 -Monto:
